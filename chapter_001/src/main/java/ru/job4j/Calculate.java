306a75a220c08326cb53c792600903ef56d0c63e
Alexander Rovnov <fast2016@mail.ru>
2018-01-04 14:51:23 +0300

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,7 +1,14 @@
 package ru.job4j;
 
-
+/**The first program.
+ *Class Calculate create Hello World.
+ *@author Alexander Rovnov
+ *@version 0.0.1
+ */
 public class Calculate{
+	/**Entry point to class & application.
+	 *param args array of string arguments.
+	 */
 	public static void main(String[] args){
 		System.out.println("Hello World"); 
 	}
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,7 @@
+/**
+ *@author Alexander Rovnov
+ *@version 0.0.1
+ *@since 0.0.01
+ */
+ 
+ package ru.job4j;
