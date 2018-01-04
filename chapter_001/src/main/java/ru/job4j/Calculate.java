8f419bdf50d6f2c46cfc27cf7ec139f9b6e601a5
Alexander Rovnov <fast2016@mail.ru>
2018-01-04 19:47:16 +0300

    1.4. JUnit [#4122] + дизайн кода

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,15 +1,24 @@
 package ru.job4j;
 
-/**The first program.
- *Class Calculate create Hello World.
- *@author Alexander Rovnov
- *@version 0.0.1
+/** The first program.
+ * Class Calculate create Hello World.
+ * @author Alexander Rovnov.
+ * @version 0.0.1
  */
 public class Calculate{
-	/**Entry point to class & application.
-	 *param args array of string arguments.
+	/** Entry point to class & application.
+	 * param args array of string arguments.
 	 */
 	public static void main(String[] args){
 		System.out.println("Hello World"); 
+	}
+	
+	/**
+	 * Method echo.
+	 * @param name Your name.
+	 * @return Echo plus your name.
+	 */
+	 public String echo(String name) {
+		 return "Echo, echo, echo : " + name;
 	}
 }
---
+++ chapter_001/src/test/java/ru/job4j/CalculateTest.java
@@ -0,0 +1,29 @@
+package ru.job4j;
+
+import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test.
+ *
+ * @author Alexander Rovnov
+ * @version 0.0.1
+ * @since 0.0.1
+ */
+ public class CalculateTest{
+	/**
+     * Test echo.
+     */ 
+	 @Test
+	 public void whenTakeNameThenTreeEchoPlusName(){
+		  String input = "Alexander Rovnov";
+		  String expect = "Echo, echo, echo : Alexander Rovnov";
+		  Calculate calc = new Calculate();
+		  String result = calc.echo(input);
+		  assertThat(result, is(expect));
+	 }
+ }
+ 
