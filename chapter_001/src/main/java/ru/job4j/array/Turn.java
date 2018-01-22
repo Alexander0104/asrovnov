5b7b8e8bb793ffc0331e74ab62248604df783ea3
Alexander Rovnov <fast2016@mail.ru>
2018-01-22 23:28:26 +0300

    5.2. Перевернуть массив. [#4441]

---
+++ chapter_001/src/main/java/ru/job4j/array/Turn.java
@@ -0,0 +1,26 @@
+package ru.job4j.array;
+
+/**
+ * class Turn.
+ * @author Alexander Rovnov.
+ * @version 1.0
+ * @since 1.0
+ */
+public class Turn {
+
+    /**
+     * Method back.
+     * @param array array numeric value int.
+     * @return reverse array.
+     */
+    public int[] back(int[] array) {
+        int rev;
+        
+        for (int index=0; index<array.length/2; index++) {
+            rev = array[array.length - index - 1];
+            array[array.length - index - 1] = array[index];
+            array[index] = rev;
+        }
+        return array;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/TurnTest.java
@@ -0,0 +1,46 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test.
+ *
+ * @author Alexander Rovnov
+ * @version 1.0
+ * @since 1.0
+ */
+public class TurnTest {
+
+    /**
+     * Test_1 back.
+     * Odd array.
+     */
+    @Test
+    public void whenTurnArrayWithOddAmountOfElementsThenTurnedArray() {
+        Turn tn = new Turn();
+
+        int[] date = {5 ,4, 3, 2, 1};
+        int[] resultArray = tn.back(date);
+        int[] expectArray = {1, 2, 3, 4, 5};
+        assertThat(resultArray, is(expectArray));
+
+    }
+
+    /**
+     * Test_2 back.
+     * Event array.
+     */
+    @Test
+    public void whenTurnArrayWithEvenAmountOfElementsThenTurnedArray() {
+        Turn tn = new Turn();
+
+        int[] date = {4, 1, 6, 2};
+        int[] resultArray = tn.back(date);
+        int[] expectArray = {2, 6, 1, 4};
+        assertThat(resultArray, is(expectArray));
+
+    }
+}
